889f320c627c2604ca72df300fd6316fc8d26c5a
Shivanna DM <119150988+shivannadm@users.noreply.github.com>
2022-12-06 23:14:09 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // This is the preprossesive directives , Header file
+
+int main() // int is the keyword , Main function starts here
+{
+    int a = 10;                        // a is a variable
+    printf("The value of a is %d", a); // displaying the a value
+    return 0;                          // returning the value
+}
